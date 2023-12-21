5b50c0a54ecf33552aa23ac211e0b68ee809fd4e
Bao Ngoc Ta <baongoc.ta04@gmail.com>
2023-12-21 17:08:36 -0500

    snake game

---
+++ p3_snake_game/main.py
@@ -0,0 +1,61 @@
+from turtle import Turtle, Screen
+import time 
+from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)     # need to call update() method to update the screen
+
+# CREATE SNAKE BODY
+
+snake = Snake()
+
+# CREATE FOOD
+
+food = Food()
+
+# CREATE SCOREBOARD
+
+scoreborad = ScoreBoard()
+
+# CONTROL THE SNAKE
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+# MOVE THE SNAKE
+    
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # DETECT COLLISION WITH FOOD
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreborad.update_score()
+
+    
+    # DETECT COLLISION WITH WALL
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        scoreborad.game_over()
+
+
+    # DETECT COLLISION WITH TAIL
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreborad.game_over()
+
+
+screen.exitonclick()
---
+++ p3_snake_game/scoreboard.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+ALIGHNMENT = "center"
+FONT = ("Arial", 16, "normal")
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.write(f"Score: {self.score}", align=ALIGHNMENT, font=FONT)
+        self.hideturtle()
+        
+    def update_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALIGHNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align=ALIGHNMENT, font=FONT)
